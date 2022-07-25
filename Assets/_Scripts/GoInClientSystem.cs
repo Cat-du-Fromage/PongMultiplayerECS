192f9b3afb06cf3ebb8f66305b2dbed0b5b679d8
Noxcaedibux <diogo.vieira-ferreira@hotmail.com>
2022-07-25 22:35:56 +0200

    first scripts

---
+++ Assets/_Scripts/GoInClientSystem.cs
@@ -0,0 +1,19 @@
+using Unity.Entities;
+using Unity.Transforms;
+
+public partial class GoInClientSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, in Rotation rotation) => {
+            // Implement the work to perform for each entity here.
+            // You should only access data that is local or that is a
+            // field on this job. Note that the 'rotation' parameter is
+            // marked as 'in', which means it cannot be modified,
+            // but allows this job to run in parallel with other jobs
+            // that want to read Rotation component data.
+            // For example,
+            //     translation.Value += math.mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
+        }).Schedule();
+    }
+}
