4e313da4d8797a946e3b669d8a0b7d7dd6222a70
Noxcaedibux <diogof-6@hotmail.com>
2022-07-28 12:41:36 +0200

    Create input and move systems

---
+++ Assets/_Scripts/PlayerInputSystem.cs
@@ -0,0 +1,61 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Scenes;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[UpdateInGroup(typeof(ClientSimulationSystemGroup))]
+public partial class PlayerInputSystem : SystemBase
+{
+    private ClientSimulationSystemGroup clientSimulationSystem;
+
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<NetworkIdComponent>();
+        clientSimulationSystem = World.GetExistingSystem<ClientSimulationSystemGroup>();
+        base.OnCreate();
+    }
+
+    private bool InitializePaddle(CommandTargetComponent targetComponent)
+    {
+        if (targetComponent.targetEntity != Entity.Null) return false;
+
+        EntityCommandBuffer commandBuffer = new(Allocator.Temp);
+        int localPlayerId = GetSingleton<NetworkIdComponent>().Value;
+        Entity targetEntity = GetSingletonEntity<CommandTargetComponent>();
+
+        Entities
+            .WithBurst()
+            .WithAll<PaddleTag>()
+            .WithNone<PlayerInput>()
+            .ForEach((Entity reqEntity, in GhostOwnerComponent ghostOwner) =>
+            {
+                if (ghostOwner.NetworkId != localPlayerId) return;
+                
+                commandBuffer.AddBuffer<PlayerInput>(reqEntity);
+                commandBuffer.SetComponent(targetEntity, new CommandTargetComponent { targetEntity = reqEntity});
+            })
+            .Run();
+        commandBuffer.Playback(EntityManager);
+
+        return true;
+    }
+
+
+    protected override void OnUpdate()
+    {
+        CommandTargetComponent paddle = GetSingleton<CommandTargetComponent>();
+        if ( InitializePaddle(paddle)) return;
+        
+        PlayerInput input = default;
+        input.Tick = clientSimulationSystem.ServerTick;
+        if (Keyboard.current.sKey.isPressed)
+            input.Vertical -= 1;
+        if (Keyboard.current.wKey.isPressed)
+            input.Vertical += 1;
+        
+        DynamicBuffer<PlayerInput> inputBuffer = EntityManager.GetBuffer<PlayerInput>(paddle.targetEntity);
+        inputBuffer.AddCommandData(input);
+    }
+}
---
+++ Assets/_Scripts/PlayerMoveSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+using Unity.Transforms;
+
+[UpdateInGroup(typeof(GhostPredictionSystemGroup))]
+public partial class PlayerMoveSystem : SystemBase
+{
+    private GhostPredictionSystemGroup ghostPredictionSystemGroup;
+    protected override void OnCreate()
+    {
+        ghostPredictionSystemGroup = World.GetExistingSystem<GhostPredictionSystemGroup>();
+        base.OnCreate();
+    }
+
+    protected override void OnUpdate()
+    {
+        uint tick = ghostPredictionSystemGroup.PredictingTick;
+        float deltatime = Time.DeltaTime;
+
+        Entities
+            .WithBurst()
+            .ForEach((DynamicBuffer<PlayerInput> inputBuffer, ref Translation translation,
+                in PredictedGhostComponent prediction) =>
+            {
+                if (!GhostPredictionSystemGroup.ShouldPredict(tick, prediction)) return;
+
+                PlayerInput input;
+                inputBuffer.GetDataAtTick(tick, out input);
+                if (input.Vertical > 0) translation.Value.y += deltatime;
+                else if (input.Vertical < 0) translation.Value.y -= deltatime;
+            })
+            .ScheduleParallel();
+    }
+}
